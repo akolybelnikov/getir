14645a5b9857bd29f64fcd14290204c1c55c2388
Andrey Kolybelnikov <a.kolybelnikov@gmail.com>
2022-02-06 12:15:36 +0100

    Send 400 on bad request and log the error

--- src/controllers/record.controller.js
+++ src/controllers/record.controller.js
@@ -1,12 +1,18 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const recordService = require('../services/record.service');
+const logger = require('../config/logger');
 
 const getRecords = catchAsync(async (req, res) => {
-  const result = await recordService.queryRecords(req.query);
-  const records = result.map((r) => ({ createdAt: r.createdAt, key: r.key, totalCount: r.totalCount }));
-  const response = { code: httpStatus.OK, msg: 'Success', records };
-  res.send(response);
+  try {
+    const result = await recordService.queryRecords(req.query);
+    const records = result.map((r) => ({ createdAt: r.createdAt, key: r.key, totalCount: r.totalCount }));
+    const response = { code: httpStatus.OK, msg: 'Success', records };
+    res.send(response);
+  } catch (e) {
+    logger.debug(e);
+    res.send(e);
+  }
 });
 
 module.exports = {
